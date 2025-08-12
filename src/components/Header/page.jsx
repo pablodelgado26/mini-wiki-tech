9d78e08e6e32595d827cb93dcee50c9c52abed4f
SarahMeuAmor <vitor.argeri@aluno.senai.br>
2025-08-12 15:51:42 -0300

    feat: add header component

--- src/components/Header/page.jsx
+++ src/components/Header/page.jsx
@@ -3,11 +3,11 @@ import styles from './header.module.css'
 export default function Header() {
     return (
         <header className={styles.header}>
-            <h1 className={styles.logo}>Mini Wiki Tech</h1>
+            <h1 className={styles.logo}>🖥️ Mini Wiki Tech</h1>
             <ul className={styles.nav}>
-                <li><a href="/" className={styles.link}>Home</a></li>
-                <li><a href="/sobre" className={styles.link}>Sobre</a></li>
-                <li><a href="/contato" className={styles.link}>Contato</a></li>
+                <li className={styles.item}><a href="/" className={styles.link}>Home</a></li>
+                <li className={styles.item}><a href="/sobre" className={styles.link}>Sobre</a></li>
+                <li className={styles.item}><a href="/contato" className={styles.link}>Contato</a></li>
             </ul>
         </header>
     )
