f434d7e2d8dfb109191454053e9830a2aff986d2
Vitor Argeri <vitor.argeri@aluno.senai.br>
2025-08-12 16:05:11 -0300

    feat: footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/page.jsx";
+import Footer from "@/components/Footer/page.jsx";
 // Configurar Google Fonts
 const inter = Inter({ 
     subsets: ["latin"],
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
             <body className={inter.className}>
                 <main>{children}</main>
             </body>
+            <Footer />
         </html>
     );
 }
--- src/components/Footer/page.jsx
+++ src/components/Footer/page.jsx
@@ -3,6 +3,20 @@ import styles from './footer.module.css'
 export default function Footer() {
     return (
         <div className={styles.footer}>
+            <p className={styles.copyright}>© 2025 🖥️ Mini Wiki Tech. Todos os direitos reservados.</p>
+            <div className={styles.listOfNames}>
+                <div className={styles.lineDiv  }>
+                    <p className={styles.girlName}>Alexandra Versani</p>
+                    <p className={styles.girlName}> Gabriela Moleta</p>
+                </div>
+                <div className={styles.lineDiv}>
+                    <p className={styles.boyName}>Miguel Sarti</p>
+                    <p className={styles.boyName}>Pablo Delgado</p>
+                    <p className={styles.boyName}>Vinícius Rocha</p>
+                    <p className={styles.boyName}>Vinícius Pereira</p>
+                    <p className={styles.boyName}>Vitor Argeri</p>
+                </div>
+            </div>
         </div>
     )
 }
